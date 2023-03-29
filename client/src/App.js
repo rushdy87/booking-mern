2502983ad68a3c801449a8609f3f045c5cd1be2d
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-03-30 02:40:10 +0300

    react and react-router-dom installiation

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Home, Hotel, List } from './pages';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
